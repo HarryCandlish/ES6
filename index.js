388928d6df90a417b8b041a31f6428a2fd9b5bad
HarryCandlish <hcnd90@gmail.com>
2019-06-18 12:41:16 +1200

    for of loops

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 module.exports = {
   esSixFunction,
-  esSixConcat
+  esSixConcat,
+  arrayIteration
 };
 
 // Arrow functions ( => )
+
+// var arr = [1, 2, 3, 4, 5];
 
 function esSixFunction(arr) {
   x = arr.map(n => n + 3);
   return x;
 }
+//   returns [4, 5, 6, 7, 8, 9]
 
 // Concatenation/String Interpolation
 
 function esSixConcat(a, b) {
   return `${a} ${b}`;
 }
+//returns :   'Hello There'
+
+// Array Iteration ES6
+
+// var arr = [1, 2, 3];
+
+function arrayIteration(arr) {
+  for (let i of arr) console.log(i);
+}
+// returns 1
+//         2
+//         3
--- test.test.js
+++ test.test.js
@@ -1,16 +1,23 @@
 const test1 = require("./index");
 
-test("map through the array and add + 3 to each item", function() {
+test("map through the array and add + 3 to each item", () => {
   const arr = [1, 2, 3, 4, 5, 6];
   const expected = [4, 5, 6, 7, 8, 9];
   const actual = test1.esSixFunction(arr);
   expect(actual).toEqual(expected);
 });
 
-test("using es6 syntax add a and b strings", function() {
+test("using es6 syntax add a and b strings", () => {
   const a = ["Hello"];
   const b = ["There"];
   const expected = "Hello There";
   const actual = test1.esSixConcat(a, b);
+  expect(actual).toEqual(expected);
+});
+
+test("use es6 loop to display the array", () => {
+  const arr = [1, 2, 3];
+  const expected = [1, 2, 3];
+  const actual = test1.arrayIteration(arr);
   expect(actual).toEqual(expected);
 });
